207eb9c75190080199ff0e28589a3c1b38b0019a
Szewek <shevkpl@gmail.com>
2020-06-17 16:37:40 +0200

    Create input for Minecraft Item IDs

---
+++ web/src/components/MCItemInput.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-combobox v-model="v" :items="items" />
+</template>
+
+<script>
+    export default {
+        name: "MCItemInput",
+        props: {value: String},
+        data: () => ({v: "", items: []}),
+        watch: {
+            v(v) {
+                this.$emit("input", v)
+            }
+        },
+    }
+</script>
