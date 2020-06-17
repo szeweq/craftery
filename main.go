b4b0a46d22aea8e0981817fa420d31b81a767b4b
Szewek <shevkpl@gmail.com>
2020-06-17 16:36:37 +0200

    Change localhost port

--- main.go
+++ main.go
@@ -8,22 +8,29 @@ import (
 	"github.com/rs/cors"
 )
 
+var localhostURL = "http://localhost:58091"
+
 func main() {
 	fmt.Println("Launching MCTool...")
 	go func() {
-		fmt.Println("Opening http://localhost:3000 ...")
-		ie := browser.OpenURL("http://localhost:3000")
+		fmt.Println("Opening http://localhost:58091 ...")
+		ie := browser.OpenURL(localhostURL)
 		if ie != nil {
 			panic(ie)
 		}
 	}()
 	initrpc()
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:8080"},
+		// Allow port 3000 for testing
+		AllowedOrigins: []string{localhostURL, "http://localhost:3000"},
 	})
 	http.HandleFunc("/ws", rpcconn)
 	http.HandleFunc("/mc/", mc.getAsset)
-	if e := http.ListenAndServe(":3000", c.Handler(http.DefaultServeMux)); e != nil {
+
+	// TEMPORARY SOLUTION!
+	http.Handle("/", http.FileServer(http.Dir("web/dist")))
+
+	if e := http.ListenAndServe(":58091", c.Handler(http.DefaultServeMux)); e != nil {
 		panic(e)
 	}
 }
--- web/src/plugins/wsrpc.js
+++ web/src/plugins/wsrpc.js
@@ -1,6 +1,6 @@
 import {Client} from 'rpc-websockets'
 
-const ws = new Client('ws://localhost:3000/ws')
+const ws = new Client('ws://localhost:58091/ws')
 
 export default class WSRPCPlugin {
     static install(Vue) {
