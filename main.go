d0a6e398981c81a8e7a03bec7cde2da6c66ae34e
Szewek <shevkpl@gmail.com>
2020-06-23 04:57:13 +0200

    Make UI handling optional

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/pkg/browser"
 	"net/http"
 
-	"github.com/pkg/browser"
 	"github.com/rs/cors"
 )
 
 var localhostURL = "http://localhost:58091"
 
+var (
+	noOpen = flag.Bool("noui", false, "Runs an app without providing UI")
+)
+
 func main() {
+	flag.Parse()
 	fmt.Println("Launching MCTool...")
-	go func() {
-		fmt.Println("Opening http://localhost:58091 ...")
-		ie := browser.OpenURL(localhostURL)
-		if ie != nil {
-			panic(ie)
-		}
-	}()
+
 	initrpc()
 	c := cors.New(cors.Options{
 		// Allow port 8080 for testing
@@ -28,8 +28,18 @@ func main() {
 	http.HandleFunc("/mc-get/", mc.getAsset)
 	http.HandleFunc("/mc-redirect/", mc.redirectAsset)
 
-	// TEMPORARY SOLUTION!
-	http.Handle("/", http.FileServer(http.Dir("web/dist")))
+	if !*noOpen {
+		// TEMPORARY SOLUTION!
+		http.Handle("/", http.FileServer(http.Dir("web/dist")))
+
+		go func() {
+			fmt.Println("Opening http://localhost:58091 ...")
+			ie := browser.OpenURL(localhostURL)
+			if ie != nil {
+				panic(ie)
+			}
+		}()
+	}
 
 	if e := http.ListenAndServe(":58091", c.Handler(http.DefaultServeMux)); e != nil {
 		panic(e)
