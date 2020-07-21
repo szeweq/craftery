63c4cc2a392badae9f6498462aa2bcede06f3279
Szewek <shevkpl@gmail.com>
2020-07-21 23:03:45 +0200

    Auto-routing

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  "transpileDependencies": [
+  pluginOptions: {
+    autoRouting: {
+      chunkNamePrefix: 'page-'
+    }
+  },
+  transpileDependencies: [
     "vuetify"
   ]
 }
