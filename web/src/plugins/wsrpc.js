07972283cd6d0aeb5d2997adc5a02dfc721f3bdb
Szewek <shevkpl@gmail.com>
2020-08-20 00:20:53 +0200

    Clean branch for different environment

--- web/src/plugins/wsrpc.js
+++
@@ -1,9 +0,0 @@
-import {Client} from 'rpc-websockets'
-
-const ws = new Client('ws://localhost:58091/ws')
-
-export default class WSRPCPlugin {
-    static install(Vue) {
-        Vue.prototype.$ws = ws
-    }
-}
