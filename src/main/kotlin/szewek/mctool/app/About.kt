cc68f05a308e8d79fd653b1165f7966e8ef17aad
Szewek <shevkpl@gmail.com>
2020-08-24 20:44:47 +0200

    About Dialog

---
+++ src/main/kotlin/szewek/mctool/app/About.kt
@@ -0,0 +1,30 @@
+package szewek.mctool.app
+
+import javafx.geometry.Pos
+import javafx.scene.layout.BorderPane
+import javafx.scene.text.Font
+import tornadofx.Fragment
+import tornadofx.*
+
+class About: Fragment("About") {
+    override val root = BorderPane()
+
+    init {
+        root.top = hbox(alignment = Pos.CENTER) {
+            padding = insets(4)
+            text("MCTool") {
+                font = Font.font(20.0)
+            }
+        }
+        root.center = text("About MCTool")
+        root.bottom = hbox(alignment = Pos.CENTER) {
+            padding = insets(4)
+            button("Close").setOnAction { this@About.close() }
+        }
+    }
+
+    fun dialog() = openModal(resizable = false)?.apply {
+        width = 200.0
+        height = 150.0
+    }
+}
