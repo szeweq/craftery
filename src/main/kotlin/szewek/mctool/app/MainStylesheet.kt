80b2c006bdafc9be0117fcc8b48aa9777f207f56
Szewek <shevkpl@gmail.com>
2020-08-26 00:39:01 +0200

    Add custom rules for formatting

--- src/main/kotlin/szewek/mctool/app/MainStylesheet.kt
+++ src/main/kotlin/szewek/mctool/app/MainStylesheet.kt
@@ -1,11 +1,34 @@
 package szewek.mctool.app
 
+import javafx.scene.text.FontWeight
 import tornadofx.*
 
 class MainStylesheet: Stylesheet() {
+    companion object {
+        val mbZero = multi(box(0.px))
+    }
+
     init {
         s(textField, button, comboBox, tab) {
-            backgroundRadius = multi(box(0.px))
+            backgroundRadius = mbZero
+        }
+        s(".tab-pane:focused > .tab-header-area > .headers-region > .tab:selected .focus-indicator") {
+            borderWidth = mbZero
+            borderRadius = mbZero
+        }
+        s(".bolder") {
+            fontWeight = FontWeight.BOLD
+        }
+        s(".page-header") {
+            padding = box(4.px)
+
+            label {
+                fontSize = 16.px
+                fontWeight = FontWeight.BOLD
+            }
+        }
+        s(".page-info") {
+            padding = box(8.px)
         }
     }
 }
