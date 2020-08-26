8601c181d1bf4496cf527e109b341bf7579600c5
Szewek <shevkpl@gmail.com>
2020-08-26 06:00:15 +0200

    New utility functions

--- src/main/kotlin/szewek/mctool/app/Funcs.kt
+++ src/main/kotlin/szewek/mctool/app/Funcs.kt
@@ -2,8 +2,10 @@ package szewek.mctool.app
 
 import javafx.scene.Node
 import javafx.scene.control.ButtonBase
-import javafx.scene.control.Tab
-import tornadofx.*
+import javafx.scene.layout.Region
+import tornadofx.UIComponent
+import tornadofx.UI_COMPONENT_PROPERTY
+import tornadofx.find
 import kotlin.reflect.KClass
 
 inline fun <reified T : UIComponent> Node.linkNewTab() = this.setOnMouseClicked {
@@ -15,3 +17,6 @@ inline fun <reified T : UIComponent> ButtonBase.linkNewTab() = this.setOnAction 
 
 inline fun <reified T : UIComponent> Node.comesFrom() = properties[UI_COMPONENT_PROPERTY] is T
 fun <T : UIComponent> Node.comesFrom(kc: KClass<T>) = kc.isInstance(properties[UI_COMPONENT_PROPERTY])
+
+fun Region.widthFrom(other: Region) = apply { prefWidthProperty().bind(other.widthProperty()) }
+fun Region.heightFrom(other: Region) = apply { prefHeightProperty().bind(other.heightProperty()) }
