119f1354c913a55baa5f9310282d7714aba6a5ae
Szewek <shevkpl@gmail.com>
2020-08-26 13:46:49 +0200

    Make separate css file

--- src/main/kotlin/szewek/mctool/app/MainStylesheet.kt
+++ src/main/kotlin/szewek/mctool/app/MainStylesheet.kt
@@ -10,39 +10,11 @@ import tornadofx.*
 class MainStylesheet: Stylesheet() {
     companion object {
         val mbZero = multi(box(0.px))
-        val loadingBox by cssclass()
-        val background by cssproperty<Color>("-fx-background")
-        val color by cssproperty<Color>("-fx-color")
-        val controlInnerBackground by cssproperty<Color>("-fx-control-inner-background")
-        val controlInnerBackgroundAlt by cssproperty<Color>("-fx-control-inner-background-alt")
-        val lightTextColor by cssproperty<Color>("-fx-light-text-color")
-        val midTextColor by cssproperty<Color>("-fx-mid-text-color")
-        val darkTextColor by cssproperty<Color>("-fx-dark-text-color")
-        val markColor by cssproperty<Color>("-fx-mark-color")
-        val markHighlightColor by cssproperty<Color>("-fx-mark-highlight-color")
+
+        inline fun <reified T> import(cssFile: String) = importStylesheet(T::class.java.getResource(cssFile).toExternalForm())
     }
 
     init {
-        val base = Color.valueOf("rgb(72, 72, 72)")
-        val col1 = Color.valueOf("rgb(40, 40, 40)")
-        val acc = Color.valueOf("rgb(68, 120, 216)")
-        root {
-            baseColor = base
-            accentColor = acc
-            focusColor = acc.derive(-0.15)
-            faintFocusColor = acc.deriveColor(0.0, 1.0, 0.85, 0.13)
-            background set base
-            color set base.derive(-0.1)
-            controlInnerBackground set col1
-            controlInnerBackgroundAlt set col1.brighter()
-            //darkTextColor set Color.valueOf("rgb(220, 220, 220)")
-            //midTextColor set Color.valueOf("rgb(100, 100, 100)")
-            //lightTextColor set col1
-            markColor set base.ladder(Stop(0.3, Color.WHITE), Stop(0.31, base.derive(-0.36)))
-        }
-        menuBar {
-            backgroundColor = multi(base.derive(0.1))
-        }
         s(textField, button, comboBox, tab) {
             backgroundRadius = mbZero
         }
@@ -50,16 +22,6 @@ class MainStylesheet: Stylesheet() {
         s(".tab-pane:bottom > .tab-header-area") { padding = box(0.px) }
         s(".tab-pane:left > .tab-header-area") { padding = box(0.px) }
         s(".tab-pane:right > .tab-header-area") { padding = box(0.px) }
-        s(".tab-pane > .tab-header-area > .tab-header-background") {
-            backgroundColor = multi(base.derive(0.1))
-        }
-        s(".tab-pane > .tab-header-area > .headers-region > .tab") {
-            backgroundInsets = multi(box(0.px, 0.px, 0.px, 0.px), box(2.px, 0.px, 0.px, 0.px))
-            backgroundColor = multi(base.derive(-0.15))
-        }
-        s(".tab-pane > .tab-header-area > .headers-region > .tab:selected") {
-            backgroundColor = multi(acc.deriveColor(0.0, 1.0, 0.85, 0.67), base)
-        }
         s(".tab-pane:focused > .tab-header-area > .headers-region > .tab:selected .focus-indicator") {
             borderWidth = mbZero
             borderRadius = mbZero
@@ -78,19 +40,6 @@ class MainStylesheet: Stylesheet() {
         }
         s(".page-info") {
             padding = box(8.px)
-        }
-        loadingBox {
-            backgroundColor = multi(c(0, 0, 0, 0.5))
-            alignment = Pos.CENTER
-
-            label {
-                backgroundColor = multi(base)
-                minWidth = 100.px
-                fontSize = 16.px
-                contentDisplay = ContentDisplay.BOTTOM
-                graphicTextGap = 8.px
-                padding = box(8.px)
-            }
         }
     }
 }
