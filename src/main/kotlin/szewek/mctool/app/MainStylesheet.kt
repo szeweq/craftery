7ae67edcfd10b7917dc8945e364e8f08560227d4
Szewek <shevkpl@gmail.com>
2020-08-26 06:54:16 +0200

    Use dark style

--- src/main/kotlin/szewek/mctool/app/MainStylesheet.kt
+++ src/main/kotlin/szewek/mctool/app/MainStylesheet.kt
@@ -11,12 +11,31 @@ class MainStylesheet: Stylesheet() {
     companion object {
         val mbZero = multi(box(0.px))
         val loadingBox by cssclass()
+        val background by cssproperty<Color>("-fx-background")
+        val color by cssproperty<Color>("-fx-color")
+        val controlInnerBackground by cssproperty<Color>("-fx-control-inner-background")
+        val controlInnerBackgroundAlt by cssproperty<Color>("-fx-control-inner-background-alt")
+        val lightTextColor by cssproperty<Color>("-fx-light-text-color")
+        val midTextColor by cssproperty<Color>("-fx-mid-text-color")
+        val darkTextColor by cssproperty<Color>("-fx-dark-text-color")
+        val topColor by cssproperty<Color>("TOP-COLOR")
     }
 
     init {
-        val base = Color.valueOf("#ececec")
+        val base = Color.valueOf("rgb(72, 72, 72)")
+        val col1 = Color.valueOf("rgb(40, 40, 40)")
         root {
             baseColor = base
+            background set base
+            //color set base.derive(-10.0)
+            controlInnerBackground set col1
+            controlInnerBackgroundAlt set col1.brighter()
+            //darkTextColor set Color.valueOf("rgb(220, 220, 220)")
+            //midTextColor set Color.valueOf("rgb(100, 100, 100)")
+            //lightTextColor set col1
+        }
+        menuBar {
+            backgroundColor = multi(base)
         }
         s(textField, button, comboBox, tab) {
             backgroundRadius = mbZero
