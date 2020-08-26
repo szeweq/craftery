bdd8a77b8fc3543a07311ad215729fed817ec46f
Szewek <shevkpl@gmail.com>
2020-08-27 01:33:35 +0200

    Rewrite Stylesheet to a CSS file

--- src/main/kotlin/szewek/mctool/app/MainStylesheet.kt
+++ src/main/kotlin/szewek/mctool/app/MainStylesheet.kt
@@ -16,28 +16,10 @@ class MainStylesheet: Stylesheet() {
         s(textField, button, comboBox, tab) {
             backgroundRadius = mbZero
         }
-        s(".tab-pane:top > .tab-header-area") { padding = box(0.px) }
-        s(".tab-pane:bottom > .tab-header-area") { padding = box(0.px) }
-        s(".tab-pane:left > .tab-header-area") { padding = box(0.px) }
-        s(".tab-pane:right > .tab-header-area") { padding = box(0.px) }
         s(".tab-pane:focused > .tab-header-area > .headers-region > .tab:selected .focus-indicator") {
             borderWidth = mbZero
             borderRadius = mbZero
         }
         progressBar { bar { backgroundRadius = mbZero } }
-        s(".bolder") {
-            fontWeight = FontWeight.BOLD
-        }
-        s(".page-header") {
-            padding = box(4.px)
-
-            label {
-                fontSize = 16.px
-                fontWeight = FontWeight.BOLD
-            }
-        }
-        s(".page-info") {
-            padding = box(8.px)
-        }
     }
 }
