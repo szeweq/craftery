c409b38b69f36d26721b3760bc10c502dfec89c7
Szewek <shevkpl@gmail.com>
2020-08-26 11:39:28 +0200

    Theme customization

--- src/main/kotlin/szewek/mctool/app/MainStylesheet.kt
+++ src/main/kotlin/szewek/mctool/app/MainStylesheet.kt
@@ -3,7 +3,7 @@ package szewek.mctool.app
 import javafx.geometry.Pos
 import javafx.scene.control.ContentDisplay
 import javafx.scene.paint.Color
-import javafx.scene.paint.Paint
+import javafx.scene.paint.Stop
 import javafx.scene.text.FontWeight
 import tornadofx.*
 
@@ -18,27 +18,47 @@ class MainStylesheet: Stylesheet() {
         val lightTextColor by cssproperty<Color>("-fx-light-text-color")
         val midTextColor by cssproperty<Color>("-fx-mid-text-color")
         val darkTextColor by cssproperty<Color>("-fx-dark-text-color")
-        val topColor by cssproperty<Color>("TOP-COLOR")
+        val markColor by cssproperty<Color>("-fx-mark-color")
+        val markHighlightColor by cssproperty<Color>("-fx-mark-highlight-color")
     }
 
     init {
         val base = Color.valueOf("rgb(72, 72, 72)")
         val col1 = Color.valueOf("rgb(40, 40, 40)")
+        val acc = Color.valueOf("rgb(68, 120, 216)")
         root {
             baseColor = base
+            accentColor = acc
+            focusColor = acc.derive(-0.15)
+            faintFocusColor = acc.deriveColor(0.0, 1.0, 0.85, 0.13)
             background set base
-            //color set base.derive(-10.0)
+            color set base.derive(-0.1)
             controlInnerBackground set col1
             controlInnerBackgroundAlt set col1.brighter()
             //darkTextColor set Color.valueOf("rgb(220, 220, 220)")
             //midTextColor set Color.valueOf("rgb(100, 100, 100)")
             //lightTextColor set col1
+            markColor set base.ladder(Stop(0.3, Color.WHITE), Stop(0.31, base.derive(-0.36)))
         }
         menuBar {
-            backgroundColor = multi(base)
+            backgroundColor = multi(base.derive(0.1))
         }
         s(textField, button, comboBox, tab) {
             backgroundRadius = mbZero
+        }
+        s(".tab-pane:top > .tab-header-area") { padding = box(0.px) }
+        s(".tab-pane:bottom > .tab-header-area") { padding = box(0.px) }
+        s(".tab-pane:left > .tab-header-area") { padding = box(0.px) }
+        s(".tab-pane:right > .tab-header-area") { padding = box(0.px) }
+        s(".tab-pane > .tab-header-area > .tab-header-background") {
+            backgroundColor = multi(base.derive(0.1))
+        }
+        s(".tab-pane > .tab-header-area > .headers-region > .tab") {
+            backgroundInsets = multi(box(0.px, 0.px, 0.px, 0.px), box(2.px, 0.px, 0.px, 0.px))
+            backgroundColor = multi(base.derive(-0.15))
+        }
+        s(".tab-pane > .tab-header-area > .headers-region > .tab:selected") {
+            backgroundColor = multi(acc, base)
         }
         s(".tab-pane:focused > .tab-header-area > .headers-region > .tab:selected .focus-indicator") {
             borderWidth = mbZero
