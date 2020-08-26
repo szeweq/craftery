3e95625795b8e9e46980625c7e073bb6e40576dd
Szewek <shevkpl@gmail.com>
2020-08-26 05:59:10 +0200

    Add LoaderPane

---
+++ src/main/kotlin/szewek/mctool/app/LoaderPane.kt
@@ -0,0 +1,40 @@
+package szewek.mctool.app
+
+import javafx.application.Platform
+import javafx.scene.control.Label
+import javafx.scene.control.ProgressBar
+import javafx.scene.layout.StackPane
+import javafx.scene.layout.VBox
+import tornadofx.*
+
+class LoaderPane: StackPane() {
+    private val progress = ProgressBar()
+    private val msg = Label()
+    private val box = VBox()
+
+    init {
+        box.addClass("loading-box")
+        box += msg
+        msg.graphic = progress
+        progress.prefWidthProperty().bind(box.widthProperty() / 2)
+    }
+
+    fun <T> launchTask(func: FXTask<*>.() -> T) {
+        children.add(box)
+        val t = task {
+            try {
+                func()
+            } finally {
+                Platform.runLater(::onTaskFinish)
+            }
+        }
+        progress.bind(t.progressProperty(), true)
+        msg.bind(t.messageProperty(), true)
+    }
+
+    private fun onTaskFinish() {
+        progress.progressProperty().unbind()
+        msg.textProperty().unbind()
+        children.remove(box)
+    }
+}
