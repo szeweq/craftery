3cbb209df0fac4b777ef425e3c5095a3d3278986
Szewek <shevkpl@gmail.com>
2020-08-27 21:51:08 +0200

    Create button for each action

--- src/main/kotlin/szewek/mctool/app/Welcome.kt
+++ src/main/kotlin/szewek/mctool/app/Welcome.kt
@@ -1,6 +1,9 @@
 package szewek.mctool.app
 
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
 import javafx.geometry.Pos
+import javafx.scene.control.Control
 import javafx.scene.layout.VBox
 import javafx.scene.text.Font
 import tornadofx.*
@@ -15,9 +18,26 @@ class Welcome: Fragment("Welcome") {
                 padding = insets(8.0)
                 font = Font.font(28.0)
             }
-            button("Lookup fields in a mod") {
-                linkNewTab<ModSearch>()
+            hbox {
+                maxWidthProperty().bind(root.widthProperty() * 0.4)
+                actions.forEach {
+                    button(it.text).apply {
+                        widthFrom(this@hbox)
+                        onAction = it.action
+                    }
+                }
             }
         }
+    }
+
+    class Action(val text: String, val action: EventHandler<ActionEvent>)
+
+    companion object {
+        val actions = observableListOf(
+            "Lookup fields in a mod" { openTab<ModSearch>() }
+        )
+
+        private operator fun String.invoke(action: EventHandler<ActionEvent>) = Action(this, action)
+        private inline fun <reified T : UIComponent> openTab() = find<MainView>().openTab<T>()
     }
 }
