91b22e4d0231110515b58fa97bad8012bc504697
Szewek <shevkpl@gmail.com>
2020-09-01 13:48:14 +0200

    Create Deserializer for InputStream

--- src/main/kotlin/szewek/mctool/util/Downloader.kt
+++ src/main/kotlin/szewek/mctool/util/Downloader.kt
@@ -1,24 +1,23 @@
 package szewek.mctool.util
 
 import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.Deserializable
 import com.github.kittinunf.fuel.core.ProgressCallback
+import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.fuel.core.response
 import com.github.kittinunf.fuel.gson.responseObject
 import com.github.kittinunf.result.getOrElse
-import com.github.kittinunf.result.getOrNull
-import java.io.ByteArrayInputStream
 import java.io.InputStream
 import java.util.zip.ZipInputStream
 
 object Downloader {
-
-    fun downloadFile(url: String, progress: ProgressCallback): InputStream {
-        val (_, _, bytes) = Fuel.get(url).responseProgress(progress).response()
-        return ByteArrayInputStream(bytes.get())
-    }
-
-    //suspend fun <T> downloadJson(url: String) {
-    //    return client.get(url)
-    //}
+    fun downloadFile(url: String, progress: ProgressCallback) = Fuel.get(url)
+            .requestProgress(progress)
+            .response(StreamDeserializer())
+            .third.getOrElse {
+                println("Err $it")
+                InputStream.nullInputStream()
+            }
 
     inline fun <reified T : Any> downloadJson(url: String) = Fuel.get(url)
             .responseObject<T>().third.getOrElse {
@@ -35,5 +34,9 @@ object Downloader {
 
     fun downloadZip(url: String, progress: ProgressCallback): ZipInputStream {
         return ZipInputStream(downloadFile(url, progress))
+    }
+
+    class StreamDeserializer: Deserializable<InputStream> {
+        override fun deserialize(response: Response) = response.body().toStream()
     }
 }
