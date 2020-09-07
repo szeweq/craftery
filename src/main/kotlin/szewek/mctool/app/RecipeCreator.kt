638a76b3d7d2a00f67cb2a988962bbbcf49867ac
Szewek <shevkpl@gmail.com>
2020-09-07 11:07:50 +0200

    Remove unused TaskStatus; Create function for initializing tasks

--- src/main/kotlin/szewek/mctool/app/RecipeCreator.kt
+++ src/main/kotlin/szewek/mctool/app/RecipeCreator.kt
@@ -4,7 +4,6 @@ import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
 import javafx.geometry.Pos
 import javafx.scene.control.ComboBox
-import javafx.scene.control.ProgressBar
 import javafx.scene.control.ScrollPane
 import javafx.scene.layout.*
 import szewek.mctool.app.recipe.CraftingView
@@ -12,53 +11,42 @@ import szewek.mctool.app.recipe.SlotView
 import szewek.mctool.app.task.createTaskQueue
 import szewek.mctool.mcdata.MinecraftData
 import szewek.mctool.mcdata.Models
-import tornadofx.TaskStatus
 import tornadofx.View
 import tornadofx.bind
-import tornadofx.visibleWhen
 
 class RecipeCreator: View("Create recipes") {
     override val root = GridPane()
     private val recipeType = SimpleStringProperty("")
     private val allRecipes = FXCollections.observableArrayList<String>()
-    private val taskStatus = TaskStatus()
 
     init {
-        createTaskQueue(
-                MinecraftData.loadAllFilesFromJar(null),
-                Models.compile()
-        )
         val pct50 = root.widthProperty().divide(2)
 
         root.addRow(0, HBox() children {
             + ComboBox(allRecipes).apply { bind(recipeType) }
-            + ProgressBar().apply {
-                bind(taskStatus.progress)
-                visibleWhen(taskStatus.running)
-            }
         })
         root.addRow(
                 1,
                 VBox(CraftingView()).apply { prefWidthProperty().bind(pct50) },
                 ScrollPane(TilePane().apply {
                     alignment = Pos.TOP_LEFT
-                } children {
-                    for (i in 0..26) {
-                        + SlotView()
-                    }
-                }).apply {
+                } children { for (i in 0..26) { + SlotView() } }).apply {
                     prefWidthProperty().bind(pct50)
                     isFitToWidth = true
                     isFitToHeight = true
                 }
         )
         root.rowConstraints.addAll(
-                RowConstraints(),
-                RowConstraints().apply {
-                    //isFillHeight = true
-                    //maxHeight = Double.MAX_VALUE
-                    vgrow = Priority.ALWAYS
-                }
+                RowConstraints(), RowConstraints().apply { vgrow = Priority.ALWAYS }
+        )
+
+        runTasks()
+    }
+
+    private fun runTasks() {
+        createTaskQueue(
+                MinecraftData.loadAllFilesFromJar(null),
+                Models.compile()
         )
     }
 }
