9cdc5bf4733eb076afcf25d347f542f0589d09e9
Szewek <shevkpl@gmail.com>
2021-05-28 16:19:37 +0200

    Link to Github repo

--- src/main/kotlin/szewek/craftery/views/About.kt
+++ src/main/kotlin/szewek/craftery/views/About.kt
@@ -2,9 +2,11 @@ package szewek.craftery.views
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,7 +19,13 @@ class About: View("About") {
     override fun content() {
         CenteredColumn(Modifier.fillMaxSize(1.0f)) {
             Text("Craftery", Modifier.padding(8.dp), fontSize = 24.sp, fontWeight = FontWeight.Bold)
-            Text("© 2020-2021 Szewek")
+            Text("© 2020-2021 Szewek", Modifier.padding(bottom = 8.dp))
+            val uriHandler = LocalUriHandler.current
+            Button(onClick = {
+                uriHandler.openUri("https://github.com/Szewek/craftery")
+            }) {
+                Text("Github")
+            }
         }
     }
 }
