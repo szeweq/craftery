e0782828e30d72e553ad639240bf5581d8ee9adb
Szewek <shevkpl@gmail.com>
2021-05-28 20:28:19 +0200

    inline centered components

--- src/main/kotlin/szewek/craftery/layout/Centered.kt
+++ src/main/kotlin/szewek/craftery/layout/Centered.kt
@@ -6,9 +6,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
-fun CenteredRow(modifier: Modifier = Modifier, content: @Composable RowScope.() -> Unit) =
+inline fun CenteredRow(modifier: Modifier = Modifier, content: @Composable RowScope.() -> Unit) =
     Row(modifier, Arrangement.Center, Alignment.CenterVertically, content)
 
 @Composable
-fun CenteredColumn(modifier: Modifier = Modifier, content: @Composable ColumnScope.() -> Unit) =
+inline fun CenteredColumn(modifier: Modifier = Modifier, content: @Composable ColumnScope.() -> Unit) =
     Column(modifier, Arrangement.Center, Alignment.CenterHorizontally, content)
