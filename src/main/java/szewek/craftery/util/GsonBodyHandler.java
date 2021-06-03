68772e9b39800c935d42a964efa480ad0b542622
Szewek <shevkpl@gmail.com>
2021-06-03 15:36:35 +0200

    Add support for Java records in Gson

--- src/main/java/szewek/craftery/util/GsonBodyHandler.java
+++ src/main/java/szewek/craftery/util/GsonBodyHandler.java
@@ -1,6 +1,7 @@
 package szewek.craftery.util;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
@@ -8,7 +9,7 @@ import java.io.InputStreamReader;
 import java.net.http.HttpResponse;
 
 public final class GsonBodyHandler {
-    private static final Gson GSON = new Gson();
+    private static final Gson GSON = new GsonBuilder().registerTypeAdapterFactory(new RecordAdapterFactory()).create();
 
     public static <T> HttpResponse.BodyHandler<T> handle(final TypeToken<T> token) {
         return ri -> HttpResponse.BodySubscribers.mapping(HttpResponse.BodySubscribers.ofInputStream(), inputStream -> {
