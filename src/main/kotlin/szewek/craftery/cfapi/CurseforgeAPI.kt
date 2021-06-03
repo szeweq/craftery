0795d21397fbb8d080b6ebf577334539311c28a4
Szewek <shevkpl@gmail.com>
2021-06-03 15:38:31 +0200

    Add type to CurseforgeAPI methods

--- src/main/kotlin/szewek/craftery/cfapi/CurseforgeAPI.kt
+++ src/main/kotlin/szewek/craftery/cfapi/CurseforgeAPI.kt
@@ -4,7 +4,7 @@ import com.google.gson.reflect.TypeToken
 
 object CurseforgeAPI {
 
-    private inline fun <reified T> getJson(path: String, params: List<Pair<String, Any>> = listOf()) =
+    private inline fun <reified T> getJson(path: String, params: List<Pair<String, Any>> = listOf()): T =
         CFAPIClient.getJson(object : TypeToken<T>() {}, path, params)
 
     fun findAddons(query: String, type: Int) =
@@ -12,6 +12,6 @@ object CurseforgeAPI {
 
     fun getAddonFiles(addonId: Int) = getJson<Array<AddonFile>>("addon/$addonId/files")
 
-    fun downloadURL(addon: Int, file: Int) = CFAPIClient.getString("addon/$addon/file/$file/download-url", listOf())
+    fun downloadURL(addon: Int, file: Int): String = CFAPIClient.getString("addon/$addon/file/$file/download-url", listOf())
 
 }
