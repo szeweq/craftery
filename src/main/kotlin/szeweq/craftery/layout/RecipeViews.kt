a95e3a12deb14243416795728bad5fd06d2965dc
Szewek <shevkpl@gmail.com>
2021-10-18 14:49:41 +0200

    Use LaunchedEffect to retrieve a model

--- src/main/kotlin/szeweq/craftery/layout/RecipeViews.kt
+++ src/main/kotlin/szeweq/craftery/layout/RecipeViews.kt
@@ -5,10 +5,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,11 +15,10 @@ import szeweq.craftery.mcdata.Models
 
 @Composable
 fun ItemSlot(name: String = "minecraft:item/golden_shovel", count: Int = 0, modifier: Modifier = Modifier) {
-    val scope = rememberCoroutineScope()
-    val model = remember(name) {
-        val x = mutableStateOf<Model>(Model.Empty)
-        scope.launch { Models.getModelOf(name).let { x.value = it } }
-        x
+    val model = remember(name) { mutableStateOf<Model>(Model.Empty) }
+    LaunchedEffect(name) {
+        val m = Models.getModelOf(name)
+        model.value = m
     }
 
     Box(modifier.size(48.dp)) {
