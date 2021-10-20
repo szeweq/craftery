c98be62da9261683169db072dd0e56451fd67baf
Szewek <shevkpl@gmail.com>
2021-10-20 14:00:58 +0200

    Import fix

--- src/main/kotlin/szeweq/craftery/views/About.kt
+++ src/main/kotlin/szeweq/craftery/views/About.kt
@@ -1,7 +1,6 @@
 package szeweq.craftery.views
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
