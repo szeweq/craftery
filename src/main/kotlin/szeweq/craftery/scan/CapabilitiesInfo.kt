bf2d5046458b77ab4136289bdbf79c33de52cf31
Szewek <shevkpl@gmail.com>
2021-11-02 00:41:58 +0100

    Use single iteration to collect supclasses and fields

--- src/main/kotlin/szeweq/craftery/scan/CapabilitiesInfo.kt
+++ src/main/kotlin/szeweq/craftery/scan/CapabilitiesInfo.kt
@@ -3,19 +3,22 @@ package szeweq.craftery.scan
 import org.objectweb.asm.tree.FieldInsnNode
 import org.objectweb.asm.tree.InsnList
 import org.objectweb.asm.tree.MethodInsnNode
-import szeweq.craftery.util.stream
-import szeweq.craftery.util.toSet
-import java.util.function.Consumer
 
-class CapabilitiesInfo(val name: String, instructions: InsnList) {
-    var supclasses: Set<String> = instructions.stream()
-        .mapMulti { t, c: Consumer<String> ->
-            if (t is MethodInsnNode && "getCapability" == t.name && name != t.owner)
-                c.accept(t.owner ?: "UNKNOWN")
-        }.toSet()
-    var fields = instructions.stream()
-        .mapMulti { t, c: Consumer<String> ->
-            if (t is FieldInsnNode && TypeNames.CAPABILITY == t.desc)
-                c.accept("${t.owner ?: "UNKNOWN"}::${t.name ?: "UNKNOWN"}")
-        }.toSet()
+class CapabilitiesInfo(
+    val name: String,
+    val supclasses: Set<String>,
+    val fields: Set<String>
+)
+
+fun CapabilitiesInfo(name: String, instructions: InsnList): CapabilitiesInfo {
+    val supclasses = mutableSetOf<String>()
+    val fields = mutableSetOf<String>()
+    for (inst in instructions) {
+        if (inst is MethodInsnNode && "getCapability" == inst.name && name != inst.owner) {
+            supclasses.add(inst.owner ?: "UNKNOWN")
+        } else if (inst is FieldInsnNode && TypeNames.CAPABILITY == inst.desc) {
+            fields.add("${inst.owner ?: "UNKNOWN"}::${inst.name ?: "UNKNOWN"}")
+        }
+    }
+    return CapabilitiesInfo(name, supclasses, fields)
 }
