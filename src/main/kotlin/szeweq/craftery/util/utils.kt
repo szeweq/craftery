d4c1fe05b9deb2b5421d8600bef84e9066c08de5
Szewek <shevkpl@gmail.com>
2021-11-03 22:59:51 +0100

    Mark "provide" functions with ReadOnlyComposable annotation

--- src/main/kotlin/szeweq/craftery/util/utils.kt
+++ src/main/kotlin/szeweq/craftery/util/utils.kt
@@ -3,6 +3,7 @@ package szeweq.craftery.util
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocal
 import androidx.compose.runtime.ProvidableCompositionLocal
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.text.TextStyle
 import com.fasterxml.jackson.core.type.TypeReference
@@ -29,9 +30,11 @@ inline fun logTime(name: String, fn: () -> Unit) {
 }
 
 @Composable
+@ReadOnlyComposable
 infix fun ProvidableCompositionLocal<TextStyle>.providesMerged(value: TextStyle) =
     this provides this.current.merge(value)
 
 @Composable
+@ReadOnlyComposable
 infix fun <T> ProvidableCompositionLocal<T>.providesFrom(other: CompositionLocal<T>) =
     this provides other.current
